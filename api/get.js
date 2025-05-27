57b7f6cea62ae9e05bc9213f4eeeaa8b934e2fbd
=OnkLabs <onklabs@gmail.com>
2025-05-27 20:08:59 +0700

    change get

--- api/get.js
+++ api/get.js
@@ -26,7 +26,7 @@ module.exports = async (req, res) => {
 
   try {
     const response = await fetch(gasEndpoint, {
-      method: 'GET',
+      method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body),
     });
